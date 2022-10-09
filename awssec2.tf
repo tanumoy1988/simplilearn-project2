e75a11dfda01698ca658df88d81d075981e07d9d
tanumoypiku <tanumoypiku@gmail.com>
2022-10-09 17:32:40 +0000

    test

--- awssec2.tf
+++ awssec2.tf
@@ -2,7 +2,7 @@ provider "aws" {
     access_key = "ASIA3FWJXNXVR37G7H7E"
     secret_key = "zRM9bUGGgjshqjFRUYXicZT1dyxtiHGzksR3zRQx"
     token = "FwoGZXIvYXdzEJH//////////wEaDCx17rlVH9r9s8zv+iK2ATqRyiFRtMX56q9bS7SRLbqc1kb3scB1AbtIq6NmZ5S02xHBCy/dsxiH86irHkJ83+ArbQOU27/0coEHSJbm3+J3V6No7WW0PGkxrax3Bo7l5eyPuMYNMDgiFeVSBhynHWT5nCQoLj4IHtRiI2dn/xGbSDUgxEFWbMmK1GysDPeytqjEA1zcffi5DzcxZLFCZvgeK8Gkz2dpquKgfHkGb+35Svp/c19AZMZwkBDuLusxsoLO8kKcKI3Hi5oGMi16w7bKZVkVqvWXbm5TdqMeGJJCRBghPLK79su/7kFWt5NkPYdGrBKnc/u6Y0w="
-    region = " us-east-1"
+    region = "us-east-1"
 }
 
 resource "aws_instance" "simplilearn_project" {
